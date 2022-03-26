41f1b8566479deca28e1feeddb40729818e4fc7f
unknown <threezinedine@gmail.com>
2022-03-26 21:25:13 +0700

    update install and run bat file

---
+++ install.bat
@@ -0,0 +1,6 @@
+start pip install virtualenv
+virtualenv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+venv\Scripts\deactivate.bat
+
---
+++ run.bat
@@ -0,0 +1,1 @@
+venv\Scripts\activate && py main.py
